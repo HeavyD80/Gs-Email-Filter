c8066980abff87e60b20ccaf1020211d935fd80b
HeavyD80 <121822020+HeavyD80@users.noreply.github.com>
2023-05-28 03:27:07 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from langchain import PromptTemplate
 from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
 
 template = """
     I am going to supply you with two paragraphs. The first paragraph will begin with "ORIGINAL EMAIL:". The second paragraph will begin with "REPLY EMAIL:".
@@ -27,17 +33,17 @@ prompt = PromptTemplate(
 def load_LLM(openai_api_key):
     """Logic for loading the chain you want to use should go here."""
     # Make sure your openai_api_key is set as an environment variable
-    llm = OpenAI(temperature=.7, openai_api_key="")
+    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
     return llm
 
 st.set_page_config(page_title="Ginger's Email Filter", page_icon=":robot:")
 st.header("Ginger's Email Filter")
-
+st.write(openai_api_key)
 st.markdown("Is your name Ginger? Do you find it difficult filtering your vulgar thoughts into a professional email response? \
             In a corporate environment you can't, or at least shouldn't, reply to emails telling people that they are \
             fucking idiots. \
             Simply vent your verbal diarrhea into the prompts below and through the miracle of science, your vulgar \
-            slander will be reformatted for professional use. ")
+            slander will be reformatted for professional use.. ")
 
 st.markdown("## Email Being Replied To:")
 
@@ -58,7 +64,7 @@ st.markdown("### Your Converted Email:")
 
 if reply_email_input:
     
-    llm = load_LLM(openai_api_key="")
+    llm = load_LLM(openai_api_key=openai_api_key)
 
     prompt_with_email = prompt.format(original_email=original_email_input, reply_email=reply_email_input)
 
